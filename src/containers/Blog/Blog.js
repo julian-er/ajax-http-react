0aab699135d7aa095970d43a3856baa8c9fd1548
Julián Rosalen <rosalenjulian@gmail.com>
2020-03-13 12:46:22 -0300

    Working with guards

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -16,6 +16,11 @@ import Posts from './Posts/Posts'
 import NewPost from '../Blog/NewPost/NewPost';
 class Blog extends Component {
 
+    state={
+        auth:false,
+    }
+
+
     render () {
             
 
@@ -48,7 +53,7 @@ class Blog extends Component {
                 {/* <Route  path="/" exact render={()=><Posts></Posts>} />        i can do that but is better use  component  */}
                 {/* i can use /post/ to select where render my page instead Switch, switch stops when find the route */}
                 <Switch>
-                    <Route path="/new-post" exact component={NewPost} />
+                    {this.state.auth ? <Route path="/new-post" exact component={NewPost} />: null}
                     <Route path="/posts" component={Posts} />
                     <Redirect from="/" to="/posts" />
                 </Switch>
